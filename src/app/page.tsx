a1aab2c18ea81644755cfc79ad2f5299563194f9
SathishTeckas <sathish@teckastechnologies.com>
2024-12-05 16:21:07 +0530

    added payment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -72,15 +72,15 @@ export default function Home() {
   return (
     <main className="relative flex flex-col w-full items-center overflow-x-hidden min-h-[100vh] overflow-x-hidden">
       {/* <Header /> */}
-      <ChatBot agentId={"67500d5fd8f7b664f8bc39e8"} accountId={address}/>
+      {/* <ChatBot agentId={agent} accountId={address}/> */}
       {/* <Script id="chatbot" data-agent-id="67500d5fd8f7b664f8bc39e8" data-account-id={"0xFf43E33C40276FEEff426C5448cF3AD9df6b5741"} src="https://chatbot-teckas.netlify.app/ChatBot.js"></Script> */}
       {/* <CreateRequestForm /> */}
       {/* <RequestTabs /> */}
-      {/* {show?( 
+      {show?( 
        <ChatBot agentId={agent} accountId={accountId}/>
       ):(
         <ChatAccessDenied/>
-      )} */}
+      )}
       {/* <SampleCode /> */}
     </main>
   );
